9ccb5cb19ac66d87fc820496416b006825bc84aa
Alyssa Yates <bosslys@icloud.com>
2019-10-29 17:34:09 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,10 +1,4 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
-  def change
-    create_table :actors do |t|
-      t.string :first_name
-      t.string :last_name
-    end 
-  end
 end
