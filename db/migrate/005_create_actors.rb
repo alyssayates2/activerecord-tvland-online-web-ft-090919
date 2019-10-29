64ef59a6412df9a816eca58c4a45c855a0a2c738
Alyssa Yates <bosslys@icloud.com>
2019-10-29 17:53:45 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_actors.rb
+++ db/migrate/005_create_actors.rb
@@ -3,10 +3,10 @@ def change
   create_table :actors do |t|
     t.string :first_name
     t.string :last_name
-    end 
+    end
   end
 
   def full_name
     ("#{first_name}, #{last_name}")
-  end 
+  end
 end
