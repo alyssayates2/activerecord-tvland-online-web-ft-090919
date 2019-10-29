44753b303e85ea3f45b25bb8ffd4e982b5e83522
Alyssa Yates <bosslys@icloud.com>
2019-10-29 19:10:27 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,9 @@ class Actor < ActiveRecord::Base
   def full_name
     self.first_name + " " + last_name
   end
+
+  def list_roles
+    self.first_name + " " + last_name + " - " + Show.name
+  end
+
 end
