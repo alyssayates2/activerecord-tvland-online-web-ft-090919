148382eb56c5ec7029cf4fb030dc5dc721d17643
Alyssa Yates <bosslys@icloud.com>
2019-10-29 19:58:49 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,8 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters.collect do |character|
-      character.name + " - " 
+      binding.pry
+      character.name + " - "
     end
   end
 
