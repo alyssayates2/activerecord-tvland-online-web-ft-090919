6265b7e05a80c24d627ab652e896869a904904a2
Alyssa Yates <bosslys@icloud.com>
2019-10-29 18:44:34 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    Actor.select('first_name AS field_one', 'last_name AS field_two')
+    self.first_name last_name
   end
 end
