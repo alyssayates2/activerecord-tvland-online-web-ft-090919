bc0d041df282b07804e93477c345af908cbaadc4
Alyssa Yates <bosslys@icloud.com>
2019-10-29 17:36:02 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,12 +1,4 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
-
-  def change
-    create_table :characters do |t|
-      t.string :name
-      t.integer :actor_id
-      t.integer :show_id
-    end 
-  end
 end
